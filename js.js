35e431af94583935353a3cc2a3a9733fdf2dd2b4
hellaed <hellaed3@gmail.com>
2025-06-20 02:36:04 +0300

    Add files via upload
    Project. Path#2

--- js.js
+++ js.js
@@ -11,13 +11,13 @@ const persons = [
     name: "Ali",
     phone: "053123456",
     email:"Ali@gmail.com",
-    image:"./image/Ali.jpg"
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcqmkbI8wq_KSBRtaIKma5qSm93z8-_Fw8vw&s"
   },
   {
     name: "Nora",
     phone: "0541234567",
     email:"Nora@gmail.com",
-    image:"./image/nora.webp"
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnoZXcOgXqyZMzBCEA4juHHENvUTkO4ZfQ6w&s"
   }
 ]
 
@@ -52,6 +52,38 @@ function closeAddContact() {
 function deleteContact(element) {
     const li = element.closest('li');
     if (li) li.remove();
+}
+function deleteAll() {
+  ul.innerHTML = ""; 
+}
+function formSub(event) {
+  event.preventDefault(); // stop form from reloading the page
+  let name = document.getElementById("input-name").value;
+  let phone = document.getElementById("input-phone").value;
+  let email = document.getElementById("input-email").value;
+  let img = document.getElementById("input-img").value;
+
+  // Add to array 
+  persons.push({
+    name: name,
+    phone: phone,
+    email: email,
+    image: img
+  });
+
+  const li = document.createElement('li');
+  li.innerHTML = `
+    <p><img src="${img}" id="profile_picture"> ${name}</p>
+    <p class="Phone">Phone: ${phone}</p>
+    <p class="Email">Email: ${email}</p>
+    <p>
+      <img src="./images/icons/rename_icon.png">
+      <img src="./images/icons/trash_icon.png" onclick="deleteContact(this)">
+      <img src="./images/icons/info_icon.png">
+    </p>`;
+    
+  ul.appendChild(li);
+  closeAddContact(); // hide the form
 }
 
 
